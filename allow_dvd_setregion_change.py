e4252bfd289845ee2ee3138cde2fccd4550e9cf3
Ryan Manly <ryan.manly@gmail.com>
2015-10-06 12:52:25 -0500

    it works

--- allow_dvd_setregion_change.py
+++ allow_dvd_setregion_change.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 
-# this is my ATTEMPT at modifying the second half of macmule's shell script here
 # https://github.com/macmule/nonadminchangeregion/blob/master/allowNonAdminToChangeDVDRegion.sh
 # with modern authdb modification techniques for einstein2 in ##osx-server
 
